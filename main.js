c7b7af4ad6543f646e7a583896fd308af828a66d
Henihajer <henihajer@yahoo.fr>
2022-12-13 20:45:47 +0100

    first project

---
+++ main.js
@@ -0,0 +1,4 @@
+$( "header" ).css({ 'background': "#ccc",'height':20 });
+$( ".baniere" ).css({ 'background': "SkyBlue",'height':100 ,'width':30});
+$('.section').css({'background-image': 'url(img/fond.jpg','height':700})
+$( "footer" ).css({ 'background': "#ccc", 'height': 20 });
